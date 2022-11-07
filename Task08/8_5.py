e789a20ee4751aeef4d9c1b4542de829cbc47f63
pe2ya <32875661+pe2ya@users.noreply.github.com>
2022-11-07 13:19:04 +0200

    Task08 upload

---
+++ Task08/8_5.py
@@ -0,0 +1,29 @@
+class A:
+
+    def __init__(self):
+        self.a_variable = True
+
+    def test(self):
+        print("A class")
+
+
+class B:
+
+    def __init__(self):
+        self.b_variable = True
+
+    def test(self):
+        print("B class")
+
+
+class C(A, B):
+    def __init__(self):
+        A.__init__(self)
+        B.__init__(self)
+
+
+
+c = C()
+c.test()
+
+print(vars(c))
